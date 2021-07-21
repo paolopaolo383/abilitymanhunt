bdb3fe031d131996d09f7af7c13a5b2ad0a148ef
paolopaolo383 <481926paolo@gmail.com>
2021-07-21 22:44:11 +0900

    뭐 이것저것 세팅은 함

--- src/main/java/hypixel/hypixel/Hypixel.java
+++ src/main/java/hypixel/hypixel/Hypixel.java
@@ -9,6 +9,7 @@ import org.bukkit.*;
 import com.google.common.util.concurrent.FutureCallback;
 
 import java.io.File;
+import java.nio.charset.StandardCharsets;
 import java.util.*;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.TimeUnit;
@@ -23,12 +24,11 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.*;
 import org.bukkit.event.block.Action;
+import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.entity.CreatureSpawnEvent;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.entity.EntityDeathEvent;
-import org.bukkit.event.player.PlayerInteractAtEntityEvent;
-import org.bukkit.event.player.PlayerInteractEvent;
-import org.bukkit.event.player.PlayerMoveEvent;
+import org.bukkit.event.player.*;
 import org.bukkit.generator.BlockPopulator;
 import org.bukkit.generator.ChunkGenerator;
 import org.bukkit.inventory.ItemStack;
@@ -45,7 +45,6 @@ import org.bukkit.command.ConsoleCommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.PlayerDeathEvent;
-import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.event.EventHandler;
 import org.bukkit.potion.PotionEffect;
@@ -61,16 +60,57 @@ import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import javax.swing.*;
-public final class Hypixel extends JavaPlugin {
-
+public final class Hypixel extends JavaPlugin implements Listener{
+    ConsoleCommandSender consol = Bukkit.getConsoleSender();
     @Override
-    public void onEnable() {
-        // Plugin startup logic
+    public void onEnable()
+    {
+        getServer().getPluginManager().registerEvents(this, this);
+        consol.sendMessage( ChatColor.AQUA + "[하이픽셀 플러그인 활성화.]");
+        getLogger().info("흠...");
+    }
+    @EventHandler
+    public void move(PlayerInteractEvent e)
+    {
+        Player player = e.getPlayer();
+        if(player.hasResourcePack())
+        {
+            player.sendMessage(Component.text("리소스팩 있음"));
+
+        }
+    }
+    @EventHandler
+    public void join(PlayerJoinEvent e)
+    {
+        consol.sendMessage( ChatColor.YELLOW + "[사람이 들어옴]");
+        Player player = e.getPlayer();
+        e.setJoinMessage("누군가 들어왔다!!!");
+        if(player.hasResourcePack())
+        {
+            player.sendMessage(Component.text("리소스팩 있음"));
+
+        }
+        else if(!player.hasResourcePack())
+        {
+            player.sendMessage(Component.text("리소스팩 없음"));
+        }
+        player.setResourcePack("https://blogattach.naver.com/ff6ae356407475c0ef0b645a6281fe8c2c768fe1/20210721_258_blogfile/481926paolo_1626870902838_87x7aT_zip/VanillaTweaks_r260907.zip");
 
     }
-
+    @EventHandler
+    public void playerchat(PlayerChatEvent e)
+    {
+        e.setCancelled(true);
+    }
+    @EventHandler
+    public void player(BlockBreakEvent e)
+    {
+        e.getPlayer()
+        e.getBlock().getType()
+    }
     @Override
-    public void onDisable() {
-        // Plugin shutdown logic
+    public void onDisable()
+    {
+
     }
 }
