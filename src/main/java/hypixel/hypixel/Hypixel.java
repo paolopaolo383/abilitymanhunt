ea3d0be52a3b7447cd0dcede455cc195d2f996cc
paolopaolo383 <481926paolo@gmail.com>
2021-08-18 20:32:51 +0900

    인증서 삭제

--- src/main/java/hypixel/hypixel/Hypixel.java
+++ src/main/java/hypixel/hypixel/Hypixel.java
@@ -5,6 +5,7 @@ import org.bukkit.*;
 import org.bukkit.attribute.Attribute;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
+import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.ConsoleCommandSender;
 import org.bukkit.enchantments.Enchantment;
 import org.bukkit.entity.EntityType;
