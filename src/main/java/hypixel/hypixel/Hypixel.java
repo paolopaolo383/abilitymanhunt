5e1472a2f83a145dea51d1b8740b60e9d5d1e27a
paolopaolo383 <481926paolo@gmail.com>
2021-08-26 09:46:31 +0900

    efqgrqaefeqf
    qefqaf2qwaGWGWRGFAFE

--- src/main/java/hypixel/hypixel/Hypixel.java
+++ src/main/java/hypixel/hypixel/Hypixel.java
@@ -1,8 +1,12 @@
 package hypixel.hypixel;
 
+import com.destroystokyo.paper.Namespaced;
+import com.google.common.collect.Multimap;
 import net.kyori.adventure.text.Component;
+import net.md_5.bungee.api.chat.BaseComponent;
 import org.bukkit.*;
 import org.bukkit.attribute.Attribute;
+import org.bukkit.attribute.AttributeModifier;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.CommandExecutor;
@@ -15,20 +19,28 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
 import org.bukkit.event.entity.PlayerDeathEvent;
 import org.bukkit.event.player.*;
+import org.bukkit.inventory.EquipmentSlot;
+import org.bukkit.inventory.ItemFlag;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.ShapedRecipe;
+import org.bukkit.inventory.meta.EnchantmentStorageMeta;
 import org.bukkit.inventory.meta.SkullMeta;
+import org.bukkit.inventory.meta.tags.CustomItemTagContainer;
+import org.bukkit.persistence.PersistentDataContainer;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
 import org.bukkit.scheduler.BukkitRunnable;
 import org.bukkit.scoreboard.*;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 import java.io.File;
 import java.util.*;
-
-public final class Hypixel extends JavaPlugin implements Listener
+public final class Hypixel extends JavaPlugin implements Listener, CommandExecutor
 {
+    public int ti;
+    public World neither;
     public String mapname = "uhc";
     public int umapid = 0;
     public int peacetime = 10;
@@ -62,7 +74,7 @@ public final class Hypixel extends JavaPlugin implements Listener
         getServer().getPluginManager().registerEvents(this, this);
         consol.sendMessage( ChatColor.AQUA + "[Hypixel] 하이픽셀 플러그인 v1.1 활성화.");
         config();
-        /*
+
         consol.sendMessage( ChatColor.AQUA + "[Hypixel] 레시피 제작중");
 
 
@@ -94,20 +106,31 @@ public final class Hypixel extends JavaPlugin implements Listener
 
 
         item = new ItemStack(Material.ENCHANTED_BOOK,1);
-        item.addUnsafeEnchantment(Enchantment.PROTECTION_ENVIRONMENTAL,1);
+        EnchantmentStorageMeta a =(EnchantmentStorageMeta) item.getItemMeta();
+        a.addStoredEnchant(Enchantment.PROTECTION_ENVIRONMENTAL,1,false);
+        item.setItemMeta(a);
         newrecipe = new ShapedRecipe(new NamespacedKey(this, "protect_book"),item).shape(new String[]{"   "," @@"," @#"}).setIngredient('@',Material.PAPER).setIngredient('#',Material.IRON_INGOT);
         getServer().addRecipe(newrecipe);
 
         item = new ItemStack(Material.ENCHANTED_BOOK,1);
-        item.addUnsafeEnchantment(Enchantment.DAMAGE_ALL,1);
+        EnchantmentStorageMeta b =(EnchantmentStorageMeta) item.getItemMeta();
+        b.addStoredEnchant(Enchantment.DAMAGE_ALL,1,false);
+        item.setItemMeta(b);
         newrecipe = new ShapedRecipe(new NamespacedKey(this, "sharp_book"),item).shape(new String[]{"Q  "," @@"," @#"}).setIngredient('@',Material.PAPER).setIngredient('Q',Material.FLINT).setIngredient('#',Material.IRON_SWORD);
         getServer().addRecipe(newrecipe);
 
+        item = new ItemStack(Material.ANVIL,1);
+        newrecipe = new ShapedRecipe(new NamespacedKey(this, "anvil"),item).shape(new String[]{"@@@"," ! ","@@@"}).setIngredient('@',Material.IRON_INGOT).setIngredient('!',Material.IRON_BLOCK);
+        getServer().addRecipe(newrecipe);
+
+
         item = new ItemStack(Material.IRON_HELMET,1);
-        item.addUnsafeEnchantment(Enchantment.PROTECTION_ENVIRONMENTAL,1);
         item.addUnsafeEnchantment(Enchantment.PROTECTION_PROJECTILE,1);
         item.addUnsafeEnchantment(Enchantment.PROTECTION_EXPLOSIONS,1);
         item.addUnsafeEnchantment(Enchantment.PROTECTION_FIRE,1);
+        a =(EnchantmentStorageMeta) item.getItemMeta();
+        a.addStoredEnchant(Enchantment.PROTECTION_ENVIRONMENTAL,1,false);
+        item.setItemMeta(a);
         newrecipe = new ShapedRecipe(new NamespacedKey(this, "special_helmat"),item).shape(new String[]{"@@@","@#@","   "}).setIngredient('@',Material.IRON_INGOT).setIngredient('#',Material.REDSTONE_TORCH);
         getServer().addRecipe(newrecipe);
 
@@ -119,11 +142,8 @@ public final class Hypixel extends JavaPlugin implements Listener
 
 
         consol.sendMessage( ChatColor.GREEN + "[Hypixel] 레시피 제작완료");
-*/
-        //item = new ItemStack(Material.COAL,1);
 
-        //newrecipe = new ShapedRecipe(item).shape(new String[]{"@@@","@@@","@@@"}).setIngredient('@',Material.PLANK);
-        //getServer().addRecipe(newrecipe);
+
         new BukkitRunnable()
         {
             @Override
@@ -131,11 +151,16 @@ public final class Hypixel extends JavaPlugin implements Listener
             {
                 if(isgaming)
                 {
-                    List<Player> players = Objects.requireNonNull(getServer().getWorld(mapname)).getPlayers();
-                    for (Player player : players) {
-                        Player pl = player;
-                        uhcscboard(pl);
+                    if(min>-1&&sec>20)
+                    {
+                        List<Player> players = getServer().getWorld(mapname).getPlayers();
+                        for (Player player : players)
+                        {
+                            Player pl = player;
+                            uhcscboard(pl);
+                        }
                     }
+
                     totaltick++;
                     tick++;
                     if (tick==20)
@@ -256,7 +281,7 @@ public final class Hypixel extends JavaPlugin implements Listener
 
         int border = (int)getServer().getWorld(mapname).getWorldBorder().getSize()/2;
         int x = (player.getLocation().getBlockX()<0?-1:1)*player.getLocation().getBlockX();
-        int z = (player.getLocation().getBlockY()<0?-1:1)*player.getLocation().getBlockY();
+        int z = (player.getLocation().getBlockZ()<0?-1:1)*player.getLocation().getBlockZ();
         if(x<z)
         {
             x = z;
@@ -322,14 +347,13 @@ public final class Hypixel extends JavaPlugin implements Listener
             if(isdropinghead&&isgaming)
             {
 
-
                 ItemStack skull = new ItemStack(Material.PLAYER_HEAD, 1);
                 SkullMeta skullMeta = (SkullMeta) skull.getItemMeta();
                 skullMeta.setDisplayName(player.getName()+"'s head");
                 skullMeta.setOwner(player.getName()+"'s head");
                 skull.setItemMeta(skullMeta);
                 skull.addUnsafeEnchantment(Enchantment.LUCK,100);
-                e.getEntity().getWorld().dropItemNaturally(e.getEntity().getLocation(),skull);
+                e.getEntity().getWorld().dropItemNaturally(e.getEntity().getLocation(),skull).setVelocity(new org.bukkit.util.Vector(0,0,0));
 
 
 
@@ -524,13 +548,7 @@ public final class Hypixel extends JavaPlugin implements Listener
         Player player = (Player) sender;
         if(command.getName().equalsIgnoreCase("uhc"))
         {
-            sender.sendMessage(ChatColor.AQUA+"[Hypixel] 월드 생성중");
-            UHCWorldcreater();
-        }
-        if(command.getName().equalsIgnoreCase("re"))
-        {
-            sender.sendMessage(ChatColor.AQUA+"[Hypixel] 게임 끝내는중");
-            setGame(player);
+            player.sendMessage(ChatColor.AQUA+"[Hypixel] 월드 생성중");
             UHCWorldcreater();
         }
 
@@ -574,124 +592,138 @@ public final class Hypixel extends JavaPlugin implements Listener
 
 
 
-
-
-
     public void UHCWorldcreater()
     {
 
-        int com = 0;
-        config();
+            ti = 0;
+            int com = 0;
+            config();
 
 
-        consol.sendMessage(ChatColor.WHITE+"-----------------UHC WORLD SETTING-----------------");
+            consol.sendMessage(ChatColor.WHITE + "-----------------UHC WORLD SETTING-----------------");
 
-        Random createRandom = new Random();
-        umapid = createRandom.nextInt(1000);
-        mapname = "uhc"+String.valueOf(umapid);
-        consol.sendMessage(ChatColor.RED+"월드 이름 : "+ChatColor.YELLOW+mapname+"\n");
+            Random createRandom = new Random();
+            umapid = createRandom.nextInt(1000);
+            mapname = "uhc" + String.valueOf(umapid);
+            consol.sendMessage(ChatColor.RED + "월드 이름 : " + ChatColor.YELLOW + mapname + "\n");
 
-        peacetime = Integer.valueOf(getConfig().getString("평화시간"));
-        killtime = Integer.valueOf(getConfig().getString("죽이는　시간"));
-        deathmatchtime = Integer.valueOf(getConfig().getString("마지막　시간"));
-        consol.sendMessage(ChatColor.AQUA+"게임시간");
-        consol.sendMessage(ChatColor.GREEN+"- " + String.valueOf(peacetime)+"분");
-        consol.sendMessage(ChatColor.YELLOW+"-- " + String.valueOf(killtime)+"분");
-        consol.sendMessage(ChatColor.RED+"--- " + String.valueOf(deathmatchtime)+"분\n");
-
-
-        borderarea[0] = Integer.valueOf(getConfig().getString("자기장　처음　넓이"));
-        borderarea[1] = Integer.valueOf(getConfig().getString("자기장　두번째　넓이"));
-        borderarea[2] = Integer.valueOf(getConfig().getString("자기장　마지막　넓이"));
-        consol.sendMessage(ChatColor.AQUA+"자기장 넓이");
-        consol.sendMessage(ChatColor.GREEN+"- " + String.valueOf(borderarea[0]));
-        consol.sendMessage(ChatColor.YELLOW+"-- " + String.valueOf(borderarea[1]));
-        consol.sendMessage(ChatColor.RED+"--- " + String.valueOf(borderarea[2])+"\n");
+            peacetime = Integer.valueOf(getConfig().getString("평화시간"));
+            killtime = Integer.valueOf(getConfig().getString("죽이는　시간"));
+            deathmatchtime = Integer.valueOf(getConfig().getString("마지막　시간"));
+            consol.sendMessage(ChatColor.AQUA + "게임시간");
+            consol.sendMessage(ChatColor.GREEN + "- " + String.valueOf(peacetime) + "분");
+            consol.sendMessage(ChatColor.YELLOW + "-- " + String.valueOf(killtime) + "분");
+            consol.sendMessage(ChatColor.RED + "--- " + String.valueOf(deathmatchtime) + "분\n");
 
 
+            borderarea[0] = Integer.valueOf(getConfig().getString("자기장　처음　넓이"));
+            borderarea[1] = Integer.valueOf(getConfig().getString("자기장　두번째　넓이"));
+            borderarea[2] = Integer.valueOf(getConfig().getString("자기장　마지막　넓이"));
+            consol.sendMessage(ChatColor.AQUA + "자기장 넓이");
+            consol.sendMessage(ChatColor.GREEN + "- " + String.valueOf(borderarea[0]));
+            consol.sendMessage(ChatColor.YELLOW + "-- " + String.valueOf(borderarea[1]));
+            consol.sendMessage(ChatColor.RED + "--- " + String.valueOf(borderarea[2]) + "\n");
 
 
-        consol.sendMessage(ChatColor.YELLOW+"월드 생성중...");
-        World world;
-        try
-        {
-            WorldCreator seed = new WorldCreator(mapname);
-            world = seed.createWorld();
-        }
-        catch(Exception e)
-        {
-            consol.sendMessage(ChatColor.RED+e.getMessage());
-            return;
-        }
-        Random per = new Random();
-        com = per.nextInt(4)+68;
-        consol.sendMessage(ChatColor.GREEN+"월드 생성완료 ("+com+"%)");
+            consol.sendMessage(ChatColor.YELLOW + "월드 생성중...");
+            World world;
+
+                WorldCreator seed = new WorldCreator(mapname);
+                world = seed.createWorld();
+            /*
+            new BukkitRunnable()
+            {
+                @Override
+                public void run(){
+                    ti++;
+                    consol.sendMessage(ChatColor.YELLOW+"됨");
+                    if(ti==20)
+                    {
+                        this.cancel();
+                    }
+                }
+            }.runTaskTimer(this,0,20);
+            while (ti==20)
+            {
+                int y = 0;
+            }
+            consol.sendMessage(ChatColor.YELLOW+"됨");
+            */
+
+                Random per = new Random();
+                com = per.nextInt(4) + 68;
+
+                consol.sendMessage(ChatColor.YELLOW + "월드 설정중...");
+                world.setPVP(false);
+                world.setDifficulty(Difficulty.EASY);
+                //world.setGameRule(GameRule.DO_DAYLIGHT_CYCLE, false);
+                world.setGameRule(GameRule.NATURAL_REGENERATION, false);
+                world.setGameRule(GameRule.DO_IMMEDIATE_RESPAWN, true);
+
+                world.getWorldBorder().setCenter(0, 0);
+                world.getWorldBorder().setDamageAmount(2);
+                world.getWorldBorder().setWarningDistance(100);
+                world.getWorldBorder().setDamageBuffer(0);
+                world.getWorldBorder().setSize(borderarea[0]);
+
+                game = "uhc";
+
+                isdropinghead = false;
+                per = new Random();
+                com = per.nextInt(4) + 73;
+                consol.sendMessage(ChatColor.GREEN + "월드 설정완료 (" + com + "%)");
 
 
-        consol.sendMessage(ChatColor.YELLOW+"월드 설정중...");
-        world.setPVP(false);
-        world.setDifficulty(Difficulty.EASY);
-        //world.setGameRule(GameRule.DO_DAYLIGHT_CYCLE, false);
-        world.setGameRule(GameRule.NATURAL_REGENERATION,false);
-        world.setGameRule(GameRule.DO_IMMEDIATE_RESPAWN,true);
-
-        world.getWorldBorder().setCenter(0,0);
-        world.getWorldBorder().setDamageAmount(2);
-        world.getWorldBorder().setWarningDistance(100);
-        world.getWorldBorder().setDamageBuffer(0);
-        world.getWorldBorder().setSize(borderarea[0]);
-
-        game = "uhc";
-
-        isdropinghead = false;
-        per = new Random();
-        com = per.nextInt(4)+73;
-        consol.sendMessage(ChatColor.GREEN+"월드 설정완료 ("+com+"%)");
+            consol.sendMessage(ChatColor.GREEN + "월드 생성완료 (" + com + "%)");
 
 
+            consol.sendMessage(ChatColor.YELLOW + "플레이어 설정중...");
+            List players = getServer().getWorld("world").getPlayers();
+            int oneper = (100 - com) / players.size();
+            for (int i = 0; i < players.size(); i++) {
 
-        consol.sendMessage(ChatColor.YELLOW+"플레이어 설정중...");
-        List players = getServer().getWorld("world").getPlayers();
-        int oneper = (100-com)/players.size();
-        for (int i = 0;i<players.size();i++)
-        {
+                Player pl = (Player) players.get(i);
 
-            Player pl = (Player) players.get(i);
+                pl.setGlowing(false);
+                createRandom = new Random();
+                int xi = createRandom.nextInt(Integer.valueOf(borderarea[0] / 2)) - Integer.valueOf(borderarea[0] / 4);
+                int zi = createRandom.nextInt(Integer.valueOf(borderarea[0] / 2)) - Integer.valueOf(borderarea[0] / 4);
 
-            pl.setGlowing(false);
-            createRandom = new Random();
-            int xi = createRandom.nextInt(Integer.valueOf(borderarea[0]/2))-Integer.valueOf(borderarea[0]/4);
-            int zi = createRandom.nextInt(Integer.valueOf(borderarea[0]/2))-Integer.valueOf(borderarea[0]/4);
-            pl.teleport(world.getHighestBlockAt(xi, zi).getLocation().add(0, 1, 0));
+                pl.teleport(world.getHighestBlockAt(xi, zi).getLocation().add(0, 1, 0));
 
-            pl.getAttribute(Attribute.GENERIC_MAX_HEALTH).setBaseValue(40);
-            pl.setGameMode(GameMode.SURVIVAL);
 
-            pl.setCompassTarget(new Location(getServer().getWorld(mapname),0,50,0));
-            pl.addPotionEffect(new PotionEffect(PotionEffectType.REGENERATION, 300, 255, true));
-            pl.addPotionEffect(new PotionEffect(PotionEffectType.DAMAGE_RESISTANCE, peacetime*120, 255, true));
+                pl.getAttribute(Attribute.GENERIC_MAX_HEALTH).setBaseValue(40);
+                pl.setGameMode(GameMode.SURVIVAL);
 
-            pl.getInventory().clear();
-            ItemStack firstitem = new ItemStack(Material.STONE_PICKAXE, 1);
-            pl.getInventory().addItem(firstitem);
+                pl.setCompassTarget(new Location(getServer().getWorld(mapname), 0, 50, 0));
+                pl.addPotionEffect(new PotionEffect(PotionEffectType.REGENERATION, 300, 255, true));
+                pl.addPotionEffect(new PotionEffect(PotionEffectType.DAMAGE_RESISTANCE, peacetime * 1200, 255, true));
 
-            firstitem = new ItemStack(Material.STONE_AXE, 1);
-            pl.getInventory().addItem(firstitem);
-            firstitem = new ItemStack(Material.STONE_SWORD, 1);
-            firstitem.addUnsafeEnchantment(Enchantment.FIRE_ASPECT, 1);
-            pl.getInventory().addItem(firstitem);
-            firstitem = new ItemStack(Material.STONE_SHOVEL, 1);
-            pl.getInventory().addItem(firstitem);
-            firstitem = new ItemStack(Material.COOKED_BEEF, 64);
-            pl.getInventory().addItem(firstitem);
-            consol.sendMessage(ChatColor.GREEN+"- "+pl.getName()+" ("+String.valueOf(oneper+com-1)+"%)");
-        }
-        consol.sendMessage(ChatColor.GREEN+"플레이어 설정 완료 (100%)");
-        consol.sendMessage(ChatColor.AQUA+"uhc 게임 시작");
-        consol.sendMessage(ChatColor.WHITE+"---------------------------------------------------");
-        isgaming = true;
+                pl.getInventory().clear();
+                ItemStack firstitem = new ItemStack(Material.STONE_PICKAXE, 1);
+                pl.getInventory().addItem(firstitem);
 
+                firstitem = new ItemStack(Material.STONE_AXE, 1);
+                pl.getInventory().addItem(firstitem);
+                firstitem = new ItemStack(Material.STONE_SWORD, 1);
+                firstitem.addUnsafeEnchantment(Enchantment.FIRE_ASPECT, 1);
+                pl.getInventory().addItem(firstitem);
+                firstitem = new ItemStack(Material.STONE_SHOVEL, 1);
+                pl.getInventory().addItem(firstitem);
+                firstitem = new ItemStack(Material.COOKED_BEEF, 64);
+                pl.getInventory().addItem(firstitem);
+                consol.sendMessage(ChatColor.GREEN + "- " + pl.getName() + " (" + String.valueOf(oneper + com - 1) + "%)");
+            }
+            consol.sendMessage(ChatColor.GREEN + "플레이어 설정 완료 (100%)");
+            consol.sendMessage(ChatColor.AQUA + "uhc 게임 시작");
+            consol.sendMessage(ChatColor.WHITE + "---------------------------------------------------");
+            isgaming = true;
     }
 
+    @EventHandler
+    public void PlayerPotalEvent(PlayerPortalEvent e)
+    {
+        e.setTo(new Location(neither,0, 50,0));
+    }
 
 }
